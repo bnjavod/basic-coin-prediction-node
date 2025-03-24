0360b6687fb89b851abe3126b20223052adb410c
bnjavod <89900651+bnjavod@users.noreply.github.com>
2025-03-25 00:21:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
 app_base_path = os.getenv("APP_BASE_PATH", default=os.getcwd())
 data_base_path = os.path.join(app_base_path, "data")
 model_file_path = os.path.join(data_base_path, "model.pkl")
-scaler_file_path = os.path.join(data_base_path, "scaler.pkl")  # Added
+scaler_file_path = os.path.join(data_base_path, "scaler.pkl")
 
-TOKEN = os.getenv("TOKEN", default="ETH").upper()
+TOKEN = os.getenv("TOKEN", default="SOL").upper()
 TRAINING_DAYS = os.getenv("TRAINING_DAYS", default="90")
-TIMEFRAME = os.getenv("TIMEFRAME", default="6h")
+TIMEFRAME = os.getenv("TIMEFRAME", default="5m")
 MODEL = os.getenv("MODEL", default="XGBoost")
 REGION = os.getenv("REGION", default="us").lower()
 if REGION in ["us", "com", "usa"]:
