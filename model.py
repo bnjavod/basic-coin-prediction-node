1f0fda624fd0c64968b7a4288275c46141ae34c5
bnjavod <89900651+bnjavod@users.noreply.github.com>
2025-03-25 00:25:15 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 import numpy as np
 import requests
+from zipfile import ZipFile  # Added missing import
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, make_scorer
